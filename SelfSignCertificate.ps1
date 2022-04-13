51861ee1e4c9fbea4ebdd6790f380e20780794c7
Matteo Zamori <matteo.zamori@outlook.com>
2022-04-13 11:03:46 +0200

    Add files via upload

--- SelfSignCertificate.ps1
+++ SelfSignCertificate.ps1
@@ -2,9 +2,9 @@ Clear-Host
 Set-Location -Path $PSScriptRoot
 
 # Input Parameters  
-$CertPassword = Read-Host 'Set a Certificate Password' -MaskInput
+$CertPassword = Read-Host 'Set a Certificate Password'
 
-$cert = New-SelfSignedCertificate -Subject 'CN=Microsoft365DSC' -CertStoreLocation 'Cert:\LocalMachine\My' -KeyExportPolicy Exportable -KeySpec Signature
+$cert = New-SelfSignedCertificate -Subject 'CN=Microsoft365DSC' -DnsName 'Microsoft365DSC' -CertStoreLocation 'Cert:\LocalMachine\My' -KeyExportPolicy Exportable -Type DocumentEncryptionCertLegacyCsp -HashAlgorithm SHA256
 $password = ConvertTo-SecureString $CertPassword -AsPlainText -Force
 Export-PfxCertificate -Cert $cert -FilePath ./M365ClientCert.pfx -Password $password | Out-Null
 Export-Certificate -Cert $cert -FilePath ./M365ClientCert.cer | Out-Null
